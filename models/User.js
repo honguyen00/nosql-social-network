015c96a9a5150a34e3be79cae809eb20c9dc9bad
honguyen00 <lehonguyen00@gmail.com>
2023-12-19 14:46:39 +1100

    user model

---
+++ models/User.js
@@ -0,0 +1,41 @@
+const { Schema, model } = require('mongoose');
+// const assignmentSchema = require('./Assignment');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String, 
+            required: true,
+            max_length: 50,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, 'Please input a valid email address!']
+        }, 
+        thoughts: [{
+            type: Schema.Types.ObjectId, 
+            ref: 'thought'
+        }],
+        friends: [{
+            type: Schema.Types.ObjectId,
+            ref: 'user'
+        }],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+userSchema.virtual('friendCount').get(function() {
+    return this.friends.length
+});
+
+const User = model('user', userSchema);
+
+module.exports = User;
